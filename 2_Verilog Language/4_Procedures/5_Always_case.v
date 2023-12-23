e89cf2cf309de68a332f5719db3e4298043bbc55
Cole Croteau <55559544+Quiibbz@users.noreply.github.com>
2023-12-23 13:28:32 -0800

    Update 5_Always_case.v

--- 2_Verilog Language/4_Procedures/5_Always_case.v
+++ 2_Verilog Language/4_Procedures/5_Always_case.v
@@ -7,7 +7,7 @@ module top_module (
     input [3:0] data3,
     input [3:0] data4,
     input [3:0] data5,
-    output reg [3:0] out   );//
+    output reg [3:0] out   );
 
     always@(*) begin  // This is a combinational circuit
         case(sel)
