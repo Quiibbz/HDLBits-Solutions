bdc8dbdade1710354d45de3623e298e017e385eb
Cole Croteau <55559544+Quiibbz@users.noreply.github.com>
2023-12-23 13:26:14 -0800

    Update 6_Module_add.v

--- 2_Verilog Language/3_Modules Hierarchy/6_Module_add.v
+++ 2_Verilog Language/3_Modules Hierarchy/6_Module_add.v
@@ -3,6 +3,7 @@ module top_module(
     input [31:0] b,
     output [31:0] sum
 );
+    
     wire o;
     add16 instance1(.a(a[15:0]), .b(b[15:0]), .cin(0), .cout(o), .sum(sum[15:0]));
     add16 instance2(.a(a[31:16]), .b(b[31:16]), .cin(o), .sum(sum[31:16]));
