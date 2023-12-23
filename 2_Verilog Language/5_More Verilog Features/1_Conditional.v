46905d0579deb25d4e46c5b267b9dacf0ee7412b
Cole Croteau <55559544+Quiibbz@users.noreply.github.com>
2023-12-23 13:29:17 -0800

    Update 1_Conditional.v

--- 2_Verilog Language/5_More Verilog Features/1_Conditional.v
+++ 2_Verilog Language/5_More Verilog Features/1_Conditional.v
@@ -1,6 +1,6 @@
 module top_module (
     input [7:0] a, b, c, d,
-    output [7:0] min);//
+    output [7:0] min);
    
     wire[7:0] intermediate_result1, intermediate_result2;
     // assign intermediate_result1 = compare? true: false;
