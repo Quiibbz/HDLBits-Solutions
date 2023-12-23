35133ce3ea4bb7afdf09b069dcf2064482a05225
Cole Croteau <55559544+Quiibbz@users.noreply.github.com>
2023-12-23 13:22:52 -0800

    Update 8_7458.v

--- 2_Verilog Language/1_Basics/8_7458.v
+++ 2_Verilog Language/1_Basics/8_7458.v
@@ -3,6 +3,7 @@ module top_module (
     output p1y,
     input p2a, p2b, p2c, p2d,
     output p2y );
+    
     assign p2y = (p2a && p2b) || (p2c && p2d);
     assign p1y = (p1a && p1c && p1b) || (p1f && p1e && p1d);
 
